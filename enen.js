64d8b6736998ffdb2f8302c5734524a93b709be2
anhb52 <71751925+anhb52@users.noreply.github.com>
2021-06-21 20:14:27 +0700

    Update enen.js

--- enen.js
+++ enen.js
@@ -89,8 +89,8 @@ class encn_Cambridge {
                         if (!eng_tran) continue;
                         let definition = '';
                         eng_tran = `<span class='eng_tran'>${eng_tran.replace(RegExp(expression, 'gi'),`<b>${expression}</b>`)}</span>`;
-                        chn_tran = `<span class='chn_tran'>${chn_tran}</span>`;
-                        let tran = `<span class='tran'>${eng_tran}${chn_tran}</span>`;
+                         //chn_tran = `<span class='chn_tran'>${chn_tran}</span>`;
+                        let tran = `<span class='tran'>${eng_tran}}</span>`;
                         definition += phrasehead ? `${phrasehead}${tran}` : `${pos}${tran}`;
 
                         // make exmaple segement
@@ -172,7 +172,7 @@ class encn_Cambridge {
                 definition += `<li class="ec">${pos}<span class="ec_chn">${def}</span></li>`;
             }
             definition += '</ul>';
-            let css = `
+           /* let css = `
                 <style>
                     span.pos  {text-transform:lowercase; font-size:0.9em; margin-right:5px; padding:2px 4px; color:white; background-color:#0d47a1; border-radius:3px;}
                     span.simple {background-color: #999!important}
@@ -185,7 +185,8 @@ class encn_Cambridge {
                 definitions: [definition],
                 audios
             });
-            return notes;
+            */
+             notes;
         }
 
         function getYDTrans(doc) {
@@ -196,7 +197,7 @@ class encn_Cambridge {
             if (!transNode) return notes;
 
             let definition = `${T(transNode)}`;
-            let css = `
+            /*let css = `
                 <style>
                     .odh-expression {
                         font-size: 1em!important;
@@ -216,6 +217,7 @@ class encn_Cambridge {
             else
                 return node.innerText.trim();
         }
+        */
     }
 
     renderCSS() {
@@ -226,11 +228,11 @@ class encn_Cambridge {
                 span.pos  {text-transform:lowercase; font-size:0.9em; margin-right:5px; padding:2px 4px; color:white; background-color:#0d47a1; border-radius:3px;}
                 span.tran {margin:0; padding:0;}
                 span.eng_tran {margin-right:3px; padding:0;}
-                span.chn_tran {color:#0d47a1;}
+                /*span.chn_tran {color:#0d47a1;}*/
                 ul.sents {font-size:0.8em; list-style:square inside; margin:3px 0;padding:5px;background:rgba(13,71,161,0.1); border-radius:5px;}
                 li.sent  {margin:0; padding:0;}
                 span.eng_sent {margin-right:5px;}
-                span.chn_sent {color:#0d47a1;}
+                /*span.chn_sent {color:#0d47a1;}*/
             </style>`;
         return css;
     }
